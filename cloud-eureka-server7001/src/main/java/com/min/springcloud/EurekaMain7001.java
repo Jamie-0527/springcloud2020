45831dfc42837cebef45d1b4dc5dbeec8d4a505b
Zhangjiamin <jiaminzh@qq.com>
2020-11-17 12:27:13 +0800

    update

--- cloud-eureka-server7001/src/main/java/com/min/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/min/springcloud/EurekaMain7001.java
@@ -1,5 +1,6 @@
 package com.min.springcloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaMain7001 {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class,args);
     }
