6a09c2be9644c095dd87b9f15cc947d616a12038
Zhangjiamin <jiaminzh@qq.com>
2020-11-19 14:15:05 +0800

    Nacos作为服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/min/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.min.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhangjiamin
+ * @date 2020/11/18 22:31
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/min/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.min.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangjiamin
+ * @date 2020/11/18 22:33
+ */
+@RestController
+@RefreshScope   //开启Nacos的动态刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
